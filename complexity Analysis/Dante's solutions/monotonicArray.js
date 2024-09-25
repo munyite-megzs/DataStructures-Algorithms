16d26163988bfab094de5f2b0ea36821f3668988
dantekadagi3 <dantekadagi3@gmail.com>
2024-09-25 07:58:25 +0300

    Monotonic Array:solved the monotonic array question using basic traditional methods

---
+++ complexity Analysis/Dante's solutions/monotonicArray.js
@@ -0,0 +1,15 @@
+var isMonotonic = function(nums) {
+    let increasing = true;
+    let decreasing = true;
+    
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] < nums[i + 1]) {
+            decreasing = false;
+        } 
+        if (nums[i] > nums[i + 1]) {
+            increasing = false;
+        }
+    }
+    
+    return increasing || decreasing;
+};
